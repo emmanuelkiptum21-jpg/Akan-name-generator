a1a91ec9f9d0f1b13552f57aa876b7071b7acbe3
Emmanuel <emmanuelkiptum21@gmail.com>
2026-02-01 23:36:27 +0300

    html

--- html/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("akanForm").addEventListener("submit", function (event) {
-  event.preventDefault();
-
-  const day = parseInt(document.getElementById("day").value);
-  const month = parseInt(document.getElementById("month").value);
-  const year = parseInt(document.getElementById("year").value);
-  const gender = document.getElementById("gender").value;
-
- 
